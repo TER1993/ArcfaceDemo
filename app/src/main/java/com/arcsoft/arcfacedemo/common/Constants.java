486374a3f76402dffd133a0cab42b22f37b44ca9
754995643 <754995643@qq.com>
2020-02-25 16:41:54 +0800

    first commit

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,17 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+
+    public static final String APP_ID = "DsjR33zaa7mJ3YDZcGMmq4AyBZQHueqcMrRAjd1rwZs8";
+    public static final String SDK_KEY = "5t7HnBu5b1FURx1zfdF1CqNavf4P578C67W27kEHDXZX";
+
+    /**
+     * IR预览数据相对于RGB预览数据的横向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int HORIZONTAL_OFFSET = 0;
+    /**
+     * IR预览数据相对于RGB预览数据的纵向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int VERTICAL_OFFSET = 0;
+}
+
